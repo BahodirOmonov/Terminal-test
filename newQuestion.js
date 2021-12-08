f5b632a4a2ad6bbdec3dd360c7512214004d730f
Bahodir Omonov <93754909+BahodirOmonov@users.noreply.github.com>
2021-12-08 19:34:46 +0500

    Updated newQuestion.js

--- newQuestion.js
+++ newQuestion.js
@@ -8,6 +8,8 @@ fs.readFile('./database/question.json', 'UTF-8', (error, value) => {
 		let obj = {}
 		let variant = ['question','A', "B", "C", "D", 'answer']
 		let k = 0
+		let check = true
+
 		const readline = rl.createInterface({
 			input: process.stdin,
 			output: process.stdout
@@ -17,13 +19,28 @@ fs.readFile('./database/question.json', 'UTF-8', (error, value) => {
 		readline.prompt()
 
 		readline.on('line', (value) => {
-			obj[variant[k]] = value
+			if(value) {
+				obj[variant[k]] = value
+				check = true
+			}
+			else if(variant[k] === 'question') {
+				console.log('\nSavolni kiritmadingiz!\n')
+				readline.setPrompt('Savolni qaytadan kiriting: ')
+				readline.prompt()
+				check = false
+			}
+			else {
+				console.log('\nVariant uchun qiymat bermadingiz!\n')
+				readline.setPrompt((variant[k] === 'answer' ? "To'g'ri" : variant[k]) + " variantni kiriting: ")
+				readline.prompt()
+				check = false
+			}
 			if(variant[k] === 'answer' && !("ABCD").includes(value)) {
 				console.log("\nNoto'g'ri qiymat kiritdingiz!")
 				console.log("To'g'ri javob uchun faqat A,B,C va D lardan birini kiritishingiz kerak!\n")
 				k--
 			}
-			if((variant.length - 1) === k) {
+			if((variant.length - 1) === k && value) {
 				data.push(obj)
 				fs.writeFile('./database/question.json', JSON.stringify(data, null, 4), (error) => {
 					if(error) console.log(error)
@@ -31,8 +48,10 @@ fs.readFile('./database/question.json', 'UTF-8', (error, value) => {
 				console.log("Savol qo'shildi!")
 				return readline.close()
 			}
-			readline.setPrompt((variant[++k] === 'answer' ? "To'g'ri" : variant[k]) + " variantni kiriting: ")
-			readline.prompt()
+			if(check) {
+				readline.setPrompt((variant[++k] === 'answer' ? "To'g'ri" : variant[k]) + " variantni kiriting: ")
+				readline.prompt()
+			}
 		})
 	}
 })
